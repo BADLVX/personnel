204c381f88a406e327031dd0ccf5a10135160557
npetronis <nicolaspetronis1@gmail.com>
2023-11-27 13:53:07 +0100

    Ajout d'une classe ExceptionArrivee

---
+++ Personnel/src/testsUnitaires/ExceptionArrivee.java
@@ -0,0 +1,14 @@
+package testsUnitaires;
+
+public class ExceptionArrivee {
+     	public ExceptionArrivee()
+	    {
+	        System.out.println("Exception ExceptionArrivee has been raised...");
+	    }
+     	public String toString()
+        {
+          return "La date d'arrivee ne peut pas etre avant la date depart ";
+        }
+	
+
+}
