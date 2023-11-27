e7a650e35aaf8d28006baa190753e1d06d8d98e0
npetronis <nicolaspetronis1@gmail.com>
2023-11-27 13:56:49 +0100

    Creation d'une classe ExceptionDepart

---
+++ Personnel/src/testsUnitaires/ExceptionDepart.java
@@ -0,0 +1,14 @@
+package testsUnitaires;
+
+public class ExceptionDepart {
+     	public ExceptionDepart()
+	    {
+	        System.out.println("Exception ExceptionDepart has been raised...");
+	    }
+     	public String toString()
+        {
+          return "La date de depart ne peut pas etre apres la date d'arrivee ";
+        }
+	
+
+}
